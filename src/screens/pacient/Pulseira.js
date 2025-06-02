3ceb3474c1e13c63880d03fe9d52a37d4a2fa26c
TalesCouti <talescouti40@gmail.com>
2025-06-02 13:59:55 -0300

    remoção de pulseira NFC

--- src/screens/pacient/Pulseira.js
+++ src/screens/pacient/Pulseira.js
@@ -37,6 +37,30 @@ export default function Pulseira() {
           },
         }
       );
+   const removerPulseira = async () => {
+  setLoading(true);
+  
+  try {
+    const token = await AsyncStorage.getItem('userToken');
+    const response = await axios.post(
+      `${API_URL}/usuario/removerPulseira`,
+      {}, // Corpo vazio, pois só precisamos do token
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    Alert.alert('Sucesso', response.data.message);
+  } catch (error) {
+    console.warn(error);
+    Alert.alert('Erro', error.response?.data?.message || 'Falha ao remover pulseira.');
+  } finally {
+    setLoading(false);
+  }
+};
+    
 
       Alert.alert('Sucesso', response.data.message);
     } catch (error) {
@@ -61,6 +85,18 @@ export default function Pulseira() {
           <Text style={styles.textoBotao}>Cadastrar Pulseira</Text>
         )}
       </TouchableOpacity>
+      <Text style={[styles.textocinza, { marginTop: 30, fontSize: 20 }]}>
+        Remover Pulseira NFC
+      </Text>
+
+      <TouchableOpacity style={styles.botao} onPress={removerPulseira} disabled={loading}>
+        {loading ? (
+          <ActivityIndicator color="#fff" />
+        ) : (
+          <Text style={styles.textoBotao}>Remover Pulseira</Text>
+        )}
+      </TouchableOpacity>
     </View>
+    
   );
 }
